6b01745dcc77768be6f3bc8b68713e568bc1be5b
Jacob Aimino <jacobaimino@gmail.com>
2024-10-23 18:52:42 -0400

    fix some spacing

--- src/components/ploy-cards/index.js
+++ src/components/ploy-cards/index.js
@@ -8,18 +8,21 @@ export default function PloyCards(props) {
             <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="md">
                 {killteam?.ploys?.strat?.map((ploy) => (
                     <Card>
-                        <Title order={3}>{ploy.ployname}</Title>
-                        <div dangerouslySetInnerHTML={{ __html: `${ploy.description}` }} />
+                        <Stack>
+                            <Title order={3}>{ploy.ployname}</Title>
+                            <div dangerouslySetInnerHTML={{ __html: `${ploy.description}` }} />
+                        </Stack>
                     </Card>
                 ))}
             </SimpleGrid>
-
             <Title order={2}>Firefight Ploys</Title>
             <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="md">
                 {killteam?.ploys?.tac?.map((ploy) => (
                     <Card>
-                        <Title order={3}>{ploy.ployname}</Title>
-                        <div dangerouslySetInnerHTML={{ __html: `${ploy.description}` }} />
+                        <Stack>
+                            <Title order={3}>{ploy.ployname}</Title>
+                            <div dangerouslySetInnerHTML={{ __html: `${ploy.description}` }} />
+                        </Stack>
                     </Card>
                 ))}
             </SimpleGrid>
