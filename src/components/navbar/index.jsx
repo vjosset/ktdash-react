1a76dfbb70f4d537eec645ddfb5236843aec42dc
Jacob Aimino <jacobaimino@gmail.com>
2024-11-01 14:05:20 -0400

    Additional navbar improvements

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,13 @@
 import {
     IconBook,
+    IconBrandDiscord,
+    IconBrandGithub,
+    IconCode,
     IconDice2,
     IconDownload,
     IconLock,
     IconSettings,
+    IconTools,
     IconUser,
     IconUsers,
 } from '@tabler/icons-react';
@@ -21,35 +25,68 @@ export function NavbarSimple(props) {
         { link: '/dashboard', label: 'Play', icon: IconDice2, loggedIn: true },
         { link: `/u/${user?.username}`, label: 'Rosters', icon: IconUsers, loggedIn: true },
         { link: '/allfactions', label: 'Factions', icon: IconBook },
+        {
+            label: 'Tools', icon: IconTools, children: [
+                { link: '/name', label: 'Name Generator', icon: IconCode }
+            ]
+        },
         { link: '/settings', label: 'Settings', icon: IconSettings },
     ];
-    const links = data.filter((link) => !link.loggedIn || loggedIn).map((item) => (
-        <NavLink
-            component={Link}
-            description={item.description}
-            data-active={location.includes(item.link) || undefined}
-            href={item.link}
-            key={item.label}
-            label={item.label}
-            onClick={() => {
-                props?.close();
-            }}
-            leftSection={<item.icon />}
-        />
-    ));
+    const renderLink = (item) => {
+        return (
+            <>
+                {!!item.children ? <NavLink
+                    component={Link}
+                    description={item.description}
+                    data-active={location.includes(item.link) || undefined}
+                    key={item.label}
+                    label={item.label}
+                    leftSection={<item.icon />}
+                >
+                    {item.children.map((child) => renderLink(child))}
+                </NavLink> : <NavLink
+                    component={Link}
+                    description={item.description}
+                    data-active={location.includes(item.link) || undefined}
+                    href={item.link}
+                    key={item.label}
+                    label={item.label}
+                    onClick={() => {
+                        props?.close();
+                    }}
+                    leftSection={<item.icon />}
+                />}
+            </>
+        )
+    }
+    const links = data.filter((link) => !link.loggedIn || loggedIn).map(renderLink);
     return (
-        <nav>
+        <Stack display="flex" flex={1}>
             <Stack gap={0}>
                 {links}
+            </Stack>
+            <Stack gap={0} className={classes.navsection}>
+                <NavLink
+                    target="_blank"
+                    href={"https://discord.gg/zyuVDgYNeY"}
+                    label="Discord"
+                    leftSection={<IconBrandDiscord />}
+                />
+                <NavLink
+                    target="_blank"
+                    href={"https://github.com/jaaimino/ktdash-react"}
+                    label="Github"
+                    leftSection={<IconBrandGithub />}
+                />
                 <PWAInstallerPrompt
                     render={({ onClick }) => (
-                        <NavLink leftSection={<IconDownload />} onClick={onClick} label="Install">
+                        <NavLink leftSection={<IconDownload />} onClick={onClick} label="App Install">
                         </NavLink>
                     )}
                     callback={() => { }}
                 />
             </Stack>
-            <Stack gap={0} className={classes.footer}>
+            <Stack gap={0} className={classes.navsection}>
                 {!loggedIn ? <>
                     <NavLink
                         component={Link}
@@ -81,6 +118,6 @@ export function NavbarSimple(props) {
                     leftSection={<IconLock component={Link} />}
                 />}
             </Stack>
-        </nav>
+        </Stack>
     );
 }
