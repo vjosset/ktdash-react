bb46c2aabb959cf1fdb18633b2d15169df172cd1
Jacob Aimino <jacobaimino@gmail.com>
2024-11-04 20:10:02 -0500

    update spotlight cache

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import { request } from "@/hooks/use-api";
 import Home from "@/page/home";
 
+export const revalidate = 3600; // invalidate cache every hour
+
 export default async function HomeRoute() {
   const spotlight = await request('/roster.php?randomspotlight=1');
 
