7cb8bd43cea26745777ec706c3e462374d11d492
vjosset <lkjasd>
2024-11-08 07:46:07 -0500

    Updates for page meta

--- src/app/r/[roster]/page.js
+++ src/app/r/[roster]/page.js
@@ -2,6 +2,22 @@ import React from "react";
 import Roster from "@/page/roster";
 import { request } from "@/hooks/use-api";
 
+export async function generateMetadata({ params }) {
+  const rosterId = (await params).roster;
+  const roster = await request(`/roster.php?rid=${rosterId}`);
+  return {
+    title: `${roster.rostername} by ${roster.username} | KTDash.app`,
+    description: `${roster.killteamname} KillTeam by ${roster.username}: ${roster.notes}`,
+    openGraph: {
+        title: `${roster.rostername} by ${roster.username} | KTDash.app`,
+        url: `https://ktdash.app/r/${rosterId}`,
+        description: `${roster.killteamname} KillTeam by ${roster.username}: ${roster.notes}`,
+        images: [`https://ktdash.app/api/rosterportrait.php?rid=${rosterId}`],
+        type: 'website'
+    }
+  }
+}
+
 export default async function RosterRoute({
     params
   }) {
--- src/app/u/[user]/page.js
+++ src/app/u/[user]/page.js
@@ -2,6 +2,22 @@ import React from "react";
 import Rosters from "@/page/rosters";
 import { request } from "@/hooks/use-api";
 
+export async function generateMetadata({ params }) {
+  const username = (await params).user;
+  const user = await request(`/user.php?username=${username}`);
+  return {
+    title: `${user.username}'s Rosters | KTDash.app`,
+    description: `KillTeam rosters by ${user.username}`,
+    openGraph: {
+        title: `${user.username}'s Rosters | KTDash.app`,
+        url: `https://ktdash.app/u/${username}`,
+        description: `${user.username}'s rosters`,
+        images: [`https://ktdash.app/api/rosterportrait.php?rid=${user.rosters[0].rosterid}`],
+        type: 'website'
+    }
+  }
+}
+
 export default async function RostersRoute({
     params
   }) {
