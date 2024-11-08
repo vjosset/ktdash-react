65cf183b68821e8b75d14264edff50add0597448
vjosset <lkjasd>
2024-11-08 09:38:41 -0500

    Add meta to Signup and Login pages

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,19 @@
 import React from "react";
 import Login from "@/page/login";
 
+export async function generateMetadata({ params }) {
+  return {
+    title: 'Log In | KTDash.app',
+    description: 'Log in to view, edit, and manage your rosters and access the dashboard to run your games.',
+    openGraph: {
+        title: 'Log In | KTDash.app',
+        url: 'https://ktdash.app/login',
+        description: 'Log in to view, edit, and manage your rosters and access the dashboard to run your games.',
+        type: 'website'
+    }
+  }
+}
+
 export default async function LoginRoute() {
     return (
         <Login />
